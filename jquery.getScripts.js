4cd7b61825aca077d55a1bad18f36964225ce171
Marc S. Brooks <marc@mbrooks.info>
2015-09-06 09:46:15 -0700

    Replaced tabs with two spaces
    Added JSDoc supported comments

--- jquery.getScripts.js
+++ jquery.getScripts.js
@@ -1,6 +1,6 @@
 /**
  *  getScripts (jQuery shorthand extended)
- *  A lightweight asynchronous JavaScript loader
+ *  A lightweight asynchronous JavaScript loader.
  *
  *  Copyright 2014, Marc S. Brooks (http://mbrooks.info)
  *  Licensed under the MIT license:
@@ -11,59 +11,78 @@
  */
 
 if (!window.jQuery || (window.jQuery && window.jQuery.fn.jquery < '1.8.3')) {
-	throw new Error('getScripts requires jQuery 1.8.3 or greater.');
+  throw new Error('jQuery.getScripts requires jQuery 1.8.3 or greater.');
 }
 
 (function($) {
-	var methods = {
-		"init": function(files, callback) {
-			return this.each(function() {
-				if (!$.isArray(files) || files.length == 0) {
-					throw new Error('Invalid array');
-				}
 
-				if (callback && !$.isFunction(callback)) {
-					throw new Error('Invalid callback function');
-				}
+  /**
+   * @namespace getScripts
+   */
+  var methods = {
 
-				var count = 0;
+    /**
+     * Create new instance of getScripts
+     *
+     * @memberof getScripts
+     * @method init
+     *
+     * @example
+     * jQuery.getScripts(files, callback);
+     *
+     * @param {Array} files
+     * @param {Function} callback
+     */
+    "init": function(files, callback) {
 
-				// execute as recursive callback
-				var _callback = function() {
-					if (files.length == count) {
-						if (callback) {
-							callback();
-						}
+      // Support method chaining.
+      return this.each(function() {
+        if (!$.isArray(files) || files.length == 0) {
+          throw new Error('Invalid array');
+        }
 
-						return true;
-					}
+        if (callback && !$.isFunction(callback)) {
+          throw new Error('Invalid callback function');
+        }
 
-					var script = files[count];
+        var count = 0;
 
-					$.getScript(script)
-						.done(_callback)
-						.fail(function(jqxhr, settings, exception) {
-							throw new Error("Script '" + script + "' failed with exception\n" + exception);
-						});
+        // Execute as recursive callback.
+        var _callback = function() {
+          if (files.length == count) {
+            if (callback) {
+              callback();
+            }
 
-					count++;
-				};
+            return true;
+          }
 
-				_callback();
-			});
-		}
-	};
+          var script = files[count];
 
-	$.fn.getScripts = function(method) {
-		if (methods[method]) {
-			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-		}
-		else
-		if (typeof method === 'object' || !method) {
-			return methods.init.apply(this, arguments);
-		}
-		else {
-			$.error('Method ' +  method + ' does not exist in jQuery.getScripts');
-		}
-	};
+          $.getScript(script)
+            .done(_callback)
+            .fail(function(jqxhr, settings, exception) {
+              throw new Error("Script '" + script + "' failed with exception\n" + exception);
+            });
+
+          count++;
+        };
+
+        _callback();
+      });
+    }
+  };
+
+  $.fn.getScripts = function(method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    }
+    else
+    if (typeof method === 'object' || !method) {
+      return methods.init.apply(this, arguments);
+    }
+    else {
+      $.error('Method ' +  method + ' does not exist in jQuery.getScripts');
+    }
+  };
 })(jQuery);
